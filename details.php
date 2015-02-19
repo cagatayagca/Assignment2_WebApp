064ed1ba45773d20ef1947e4e4a146b059c7dc6d
cagatayagca <cagatayagca@gmail.com>
2015-02-19 16:35:43 +0200

    adding db queires for spesific post on blog

--- details.php
+++ details.php
@@ -13,3 +13,39 @@
    .yorum{border:0; border-top:1px dashed silver}
   </style>
  </head>
+
+ <body>
+<a href="index.php">MAIN PAGE</a><br />
+<?php
+$db = @new mysqli('localhost', 'root', '1234', 'uygulama');
+if ($db->connect_errno) die('Bağlantı Hatası:' . $db->connect_error);
+
+$db->set_charset("utf8");
+
+$blog  = $db->prepare("SELECT * FROM blog WHERE blog_id = ?");
+
+$yorum = $db->prepare("SELECT * FROM yorum WHERE blog_id = ?");
+
+$istek = isset($_GET['id']) ? $_GET['id'] : die('Hatalı istek');
+
+$blog->bind_param("i", $istek);
+
+$blog->execute();
+
+$blog_sonuc = $blog->get_result();
+
+while ($row = $blog_sonuc->fetch_array()) {
+    echo "<div class='makale'>
+         <h3>{$row['baslik']}</h3>
+         <i>{$row['tarih']} date</i>
+         <p> {$row['yazi']}  </p>";
+
+    $yorum->bind_param('i', $row['blog_id']);
+
+    $yorum->execute();
+
+    $yorum_sonuc = $yorum->get_result();
+
+    if ($yorum_sonuc->num_rows) {
+        echo '<hr /><p>'.$yorum_sonuc->num_rows . ' comments</p>';
+    }
