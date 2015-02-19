2b39b49051598657fa952525db75f2e775c8ff91
cagatayagca <cagatayagca@gmail.com>
2015-02-19 16:47:46 +0200

    check for session, 1 admin 2 other and some styles

--- admin.php
+++ admin.php
@@ -6,3 +6,26 @@ if ($db->connect_errno)  die('ERROR' . $db->connect_error);
 
 
 $db->set_charset("utf8");
+
+if(isset($_SESSION['uye'])){
+  if($_SESSION['uye'] == 2){
+     header('Location: index.php');
+  }
+}else{
+  header('Location: login.php');
+  exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="tr">
+ <head>
+  <title> My Blog </title>
+  <meta charset="utf-8" />
+  <style>
+   body{font-family: "Times New Roman", arial; font-size:12pt;}
+   div{border:1px solid silver; margin:4px; padding:4px}
+   .yorum div{border:0; border-top:1px solid silver}
+</style>
+ </head>
+ <body>
+ <a href="index.php">MAIN</a><br />
