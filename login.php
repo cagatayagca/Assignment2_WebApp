187e93723f8f2650aa62b401069d138c999ffd0b
cagatayagca <cagatayagca@gmail.com>
2015-02-19 17:38:09 +0200

    necessary db queries and interface for login

--- login.php
+++ login.php
@@ -7,3 +7,40 @@ if ($db->connect_errno)  die('ERROR' . $db->connect_error);
 
 
 $db->set_charset("utf8");
+
+$stmt  = $db->prepare("SELECT * FROM uye WHERE email=? AND sifre=MD5(?)");
+if ($stmt === false) die('ERROR'. $db->error);
+
+$stmt->bind_param("ss", $_POST['eposta'],$_POST['sifre']);
+
+$stmt->execute();
+
+
+$sonuc = $stmt->get_result();
+
+
+if($sonuc->num_rows){
+   $row = $sonuc->fetch_array();
+   $_SESSION['uye'] = $row['durum'];
+   $_SESSION['ad']  = $row['ad'];
+   header('Location: admin.php');
+  }else{
+    $hata='<h3>Wrong e-mail or password</h3>';
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="tr">
+ <head>
+  <title> LOGIN </title>
+  <meta charset="utf-8" />
+ </head>
+ <body>
+ <h2>Giriş Yap</h2>
+ <?php echo $hata; ?>
+<form method="post" action="">
+  <input type="text" name="eposta" />E-mail<br />
+  <input type="text" name="sifre" />Pass<br />
+  <input type="submit" value="Giriş" />
+</form>
+</body>
